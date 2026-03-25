cdffebcea170cd9d7500e88a7ddab838b951f36f
xcia4899 <xcia48996531@gmail.com>
2026-03-25 15:57:10 +0800

    圖片路徑設定

--- vite.config.ts
+++ vite.config.ts
@@ -2,21 +2,26 @@ import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import path from "node:path";
 
-export default defineConfig({
-  base: (import.meta as any).env.VITE_BASE_PATH || "/",
-  plugins: [vue()],
-  resolve: {
-    alias: { "@": path.resolve(__dirname, "src") }
-  },
-  css: {
-    preprocessorOptions: {
-      scss: {
-        // 這段會自動注入到每個 <style lang="scss"> 與 .scss 檔案
-        additionalData: `
-          @use "@/assets/styles/variables" as *;
-          @use "@/assets/styles/mixins" as *;
-        `
-      }
-    }
-  }
+export default defineConfig(({ mode }) => {
+  const isProd = mode === "production";
+
+  return {
+    base: isProd ? "/samphone-vue3/" : "/",
+    plugins: [vue()],
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "src"),
+      },
+    },
+    css: {
+      preprocessorOptions: {
+        scss: {
+          additionalData: `
+            @use "@/assets/styles/variables" as *;
+            @use "@/assets/styles/mixins" as *;
+          `,
+        },
+      },
+    },
+  };
 });
